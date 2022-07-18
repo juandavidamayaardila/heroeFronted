e9717fdc5180a26d30606bc52dd8fe01e8fc4dbb
JD <juandavidamayaardila@gmail.com>
2022-07-18 12:27:39 -0500

    firts commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,3 +14,21 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMomxE8GR4HSUwiQdwdOEudkT7UOeWEsM",
+  authDomain: "heroe-93825.firebaseapp.com",
+  projectId: "heroe-93825",
+  storageBucket: "heroe-93825.appspot.com",
+  messagingSenderId: "1065290400751",
+  appId: "1:1065290400751:web:12f7703d75fff8fe812c1a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
